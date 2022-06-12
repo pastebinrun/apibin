cce531d9385abc9b77b0c06b72be405b97175a0f
xfix <konrad@borowski.pw>
2022-06-12 17:06:09 +0000

    Deploying to gh-pages from @ pastebinrun/apibin@2963e7b33f6dca8a7748f1da988951c784225386 🚀

---
+++ precache-manifest.2e69949041720740d754f2dca86ee7c1.esm.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/apibin/assets/icon.png"
+  },
+  {
+    "revision": "99f8551a66a8c59febd0",
+    "url": "/apibin/bundle.7d4ab.css"
+  },
+  {
+    "revision": "f22d03d59697b218a1518a0181b842f3",
+    "url": "/apibin/bundle.d09f9.esm.js"
+  },
+  {
+    "revision": "ed4d90fc828a56366bf73d565c221326",
+    "url": "/apibin/index.html"
+  },
+  {
+    "revision": "6f924e6d8b2eafe15e908721049a325b",
+    "url": "/apibin/polyfills.c4e4e.esm.js"
+  },
+  {
+    "revision": "ef02bfd6966e942e653afbf883eb5fae",
+    "url": "/apibin/route-about.chunk.0d59d.esm.js"
+  },
+  {
+    "revision": "1939fce4207f915bb9bc",
+    "url": "/apibin/route-about.chunk.43396.css"
+  },
+  {
+    "revision": "0b3f5ab888789acefd6f",
+    "url": "/apibin/route-home.chunk.0c701.css"
+  },
+  {
+    "revision": "50ea098aeb667bf57c092eac7e691ae3",
+    "url": "/apibin/route-home.chunk.f51c3.esm.js"
+  },
+  {
+    "revision": "f300b7fd4f86d497faa3c3a83c0522e3",
+    "url": "/apibin/route-paste.chunk.1a91c.esm.js"
+  },
+  {
+    "revision": "3492af4b21e8c7e1dc6e",
+    "url": "/apibin/route-paste.chunk.ee4bc.css"
+  }
+]);
