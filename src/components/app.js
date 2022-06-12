a8fe7dc1ce660ac5333239392b1f40cc3bfdb821
Konrad Borowski <konrad@borowski.pw>
2022-06-12 19:40:03 +0200

    Recreate application with new preact-cli

--- src/components/app.js
+++ src/components/app.js
@@ -1,30 +1,26 @@
-import { h, Component } from 'preact';
+import { h } from 'preact';
 import { Router } from 'preact-router';
 import { createHashHistory } from 'history';
 
 import Header from './header';
 import Contents from './contents';
 
-// Code-splitting is automated for routes
+// Code-splitting is automated for `routes` directory
 import Home from '../routes/home';
 import About from '../routes/about';
 import Paste from '../routes/paste';
 
-export default class App extends Component {
-	handleRoute = e => {
-		this.currentUrl = e.url;
-	}
-
-	render() {
-		return (<div id="app">
+const App = () => (
+	<div id="app">
 			<Header />
 			<Contents>
-				<Router onChange={this.handleRoute} history={createHashHistory()}>
+				<Router history={createHashHistory()}>
 					<Home path="/" />
 					<About path="/about" />
 					<Paste path="/:id" />
 				</Router>
 			</Contents>
-		</div>);
-	}
-}
+		</div>
+)
+
+export default App;
